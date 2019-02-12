5ffb2f0a5cb179699813a1d930a8cbf1cf8f457b
qianxiaoming <mailtoantares@163.com>
2019-02-12 20:15:28 +0800

    Add Go source code and template files for generating namespace and storage yaml files

---
+++ bootstrap/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	var (
+		help bool
+		command string
+		sourceDir string
+		targetDir string
+		valueFile string
+	)
+	flag.BoolVar(&help, "h", false, "Show this messages")
+	flag.StringVar(&command, "c", "init", "Command to be executed")
+	flag.StringVar(&sourceDir, "s", "", "Source directory containing template YAML files")
+	flag.StringVar(&targetDir, "t", "", "Target directory to save generated kubernetes YAML files")
+	flag.StringVar(&valueFile, "v", "", "Configuration values used to generate kubernetes YAML files")
+	flag.Parse()
+	if help {
+		flag.Usage()
+		return
+	}
+
+	if command == "init" && !GenerateYAMLs(sourceDir, targetDir, valueFile) {
+		os.Exit(1)
+	}
+}
+
+type VolumeInfo struct {
+	Indices []int
+	Size string
+	Path string
+}
+
+type NodeLabels struct {
+	Service string
+	Storage string
+	Gateway string
+}
+
+type DeploymentConfig struct {
+	Namespace string `yaml:"namespace"`
+	LabelConfig NodeLabels `yaml:"labelConfig"`
+	ConfigVolume VolumeInfo `yaml:"configVolume"`
+	StreamVolume VolumeInfo `yaml:"streamVolume"`
+	AnalysisVolume VolumeInfo `yaml:"analysisVolume"`
+}
+
+func GenerateYAMLs(sourceDir string, targetDir string, valueFile string) bool {
+	info, err := os.Stat(sourceDir)
+	if err != nil || !info.IsDir() {
+		log.Printf("%s is not accessible or is not a directory", sourceDir)
+		return false
+	}
+	info, err = os.Stat(targetDir)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return false
+		} else {
+			err = os.MkdirAll(targetDir, os.ModePerm)
+			if err != nil {
+				log.Printf("Cannot create target directory %s: %v", targetDir, err)
+				return false
+			}
+		}
+	} else if !info.IsDir() {
+		log.Printf("%s is not a directory", sourceDir)
+		return false
+	}
+	info, err = os.Stat(valueFile)
+	if err != nil {
+		log.Printf("%s is not accessible or does not exist", valueFile)
+		return false
+	}
+
+	config := new(DeploymentConfig)
+	file, err := ioutil.ReadFile(valueFile)
+	if err != nil {
+		log.Printf("Unable to read value file: %v", err)
+		return false
+	}
+	err = yaml.Unmarshal(file, config)
+	if err != nil {
+		log.Fatalf("Parse value file failed: %v", err)
+	}
+
+	err = filepath.Walk(sourceDir, func(path string, f os.FileInfo, err error) error {
+		if f == nil { return err }
+		if f.IsDir() { return nil }
+		if !strings.HasSuffix(path, ".tmpl") { return nil }
+
+		tmplFile, yamlFile := path, targetDir + string(os.PathSeparator) + path[len(sourceDir)+1:]
+		yamlFile = yamlFile[0:len(yamlFile)-5]
+		fmt.Printf("Generating %s...\n", yamlFile)
+
+		file, err := os.Create(yamlFile)
+		if err != nil {
+			log.Fatalf("Cannot create output yaml file %s: %v", yamlFile, err)
+		}
+		defer file.Close()
+
+		t := template.Must(template.ParseFiles(tmplFile))
+		t.Execute(file, config)
+		file.Close()
+		return nil
+	})
+	return err == nil
+}
